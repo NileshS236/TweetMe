b7f685fd0d6ea250b604c639a10faa7264ab2e31
NileshS236 <nileshsamota0602@gmail.com>
2020-12-22 23:43:38 +0530

    another commit

--- tweets/forms.py
+++ tweets/forms.py
@@ -2,16 +2,16 @@ from django import forms
 from .models import Tweet
 
 
+MAX_TWEET_LENGTH = 240
+
+
 class TweetForm(forms.ModelForm):
-
-    MAX_TWEET_LENGTH = 240
-
     class Meta:
         model = Tweet
         fields = ["content"]
 
     def clean_content(self):
         content = self.cleaned_data.get("content")
-        if len(content) > self.MAX_TWEET_LENGTH:
+        if len(content) > MAX_TWEET_LENGTH:
             raise forms.ValidationError("This tweet is too long.")
         return content
